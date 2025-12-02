3cfbf206542a3d271722141d002718c470ec1729
Vincent-IDAPP <vincent@idapp.pro>
2025-12-02 11:58:44 +0100

    remove log env

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        console.log(`url de la page dans les var env ${process.env.MONGO_URI}`);
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
