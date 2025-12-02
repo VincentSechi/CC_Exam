616860cf48e66cfa133b5b9468fd406742969ec8
Vincent-IDAPP <vincent@idapp.pro>
2025-12-02 09:39:18 +0100

    first commit

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+// backend/middlewares/authMiddleware.js
+const jwt = require('jsonwebtoken');
+
+exports.authenticateToken = (req, res, next) => {
+
+  const token = req.headers['authorization']?.split(' ')[1];
+  console.log(`token is ${token}`)
+  //return res.status(401).json({ message: 'Accès interdits', token:token });
+  if (!token) return res.sendStatus(401);
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        console.error('Erreur de vérification du token upd');
+        return res.status(403).json({ error: true, message: 'Token invalide upd' });
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error) { 
+    return res.status(403).json({ error: true, message: 'Token invalide' });
+  }
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.user.role !== 'admin') return res.status(403).json({ message: 'Accès interdit' });
+  next();
+};
